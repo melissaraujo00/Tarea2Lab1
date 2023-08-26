b86af7d1ef5e1602988a535a687454d82ecb5ed4
melissaraujo00 <140029893+melissaraujo00@users.noreply.github.com>
2023-08-25 19:25:50 -0600

    Add files via upload

--- ProgramacionEstructurada/Laboratorio1/Tarea2/main.js
+++ ProgramacionEstructurada/Laboratorio1/Tarea2/main.js
@@ -1,13 +1,19 @@
 const cuboCifra = (num) =>{
+    cubo =[];
     cifra = Array.from(num); //convertir a array
-    cuboDigitos = cifra.map((cifra) => cifra ** 3); //cada elemento se pueda elevar al cubo
-    return cuboDigitos
+    cuboDigitos = cifra.map(cifra => Math.pow(cifra, 3));
+    cubo = Array.from(cuboDigitos)
+    return cubo
 }
 
 const numero = prompt("Ingrese un numero: ") 
-if (isNaN(numero)) {
-    alert("El valor ingresado es invalido");
-}else{
-    alert("El cubo de cada cifra de " + numero + " = " + cuboCifra(numero).join(" , "));
-}
+    if (isNaN(numero)) {
+        alert("El valor ingresado es invalido");
+    
+    }else{
+        const total = cuboCifra(numero);
+        alert(`El cubo de cada cifra de ${numero} es:  ${total.join(" , ")} `)
+        console.log("El cubo de cada cifra de ",numero, "es",total); //aqui se muestra que si esta en un array :D
+        
+    }
 
